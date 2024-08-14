a7569e41a9644cc2499550b0544e631aad2d2130
NicolasKowal <nicolas.kowal95@gmail.com>
2024-08-14 20:36:40 -0300

    .

--- refugio-austral/src/Scroll.jsx
+++ refugio-austral/src/Scroll.jsx
@@ -4,8 +4,10 @@ import { useLocation } from "react-router-dom";
 function Scroll() {
 	const { pathname } = useLocation();
 	useEffect(() => {
+		console.log("Route changed to:", pathname);
 		window.scrollTo(0, 0);
 	}, [pathname]);
+
 	return null;
 }
 
